fb0363af560408468977788c8eef4d6fcc89588a
shiferqueen <424268118@qq.com>
2017-05-22 11:40:45 +0800

    建立了部分公共的less

--- src/components/ahead.vue
+++ src/components/ahead.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div class="ahead-welcome">
-
+      <div class="aw-containter">
+        <p>欢迎来到信达</p>
+      </div>
     </div>
   </div>
 </template>
@@ -17,5 +19,10 @@ export default {
   width: 100%;
   height: 35px;
   background-color: #f2f2f2;
+  .aw-containter{
+    width: 1200px;
+    height: 35px;
+    margin:0 auto;
+  }
 }
 </style>
