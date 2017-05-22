114f0a5e78fa100fb85af89d24c2ea5a808888bc
王永盛 <1090469606@qq.com>
2017-05-22 11:57:17 +0800

    ysdown

--- src/router/index.js
+++ src/router/index.js
@@ -5,33 +5,35 @@ import Common from '@/views/Common'
 import Home from '@/views/Home'
 import Action from '@/views/Action'
 import Register from '@/views/Register'
+import shoplist from '@/views/shoplist'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Common',
-      component: Common,
-      children:[
+    routes: [{
+            path: '/',
+            name: 'Common',
+            component: Common,
+            children: [{
+                    path: 'Home',
+                    name: 'Home',
+                    component: Home
+                },
+                {
+                    path: "shoplist",
+                    name: "shoplist",
+                    component: shoplist
+                }
+            ]
+        },
         {
-          path:'Home',
-          name:'Home',
-          component:Home
+            path: '/action',
+            name: 'Action',
+            component: Action,
+            children: [{
+                path: 'register',
+                name: 'register',
+                component: Register //enroll
+            }]
         }
-      ]
-    },
-    {
-      path: '/action',
-      name: 'Action',
-      component: Action,
-      children:[
-        {
-          path:'register',
-          name:'register',
-          component:Register//enroll
-        }
-      ]
-    },
-  ]
+    ]
 })
---
+++ src/views/shoplist.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="wid">
+        <p>首页/公司工商</p>
+        <div>
+            <div>服务区域</div>
+            <div>
+                <select></select>
+                <select></select>
+                <select></select>
+            </div>
+        </div>
+        <div>
+            <div>产品类型</div>
+            <div></div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import afoot from '../components/afoot'
+ export default {
+   name: 'shoplist',
+   data() {
+     return {
+      
+    }
+  },
+//   components:{
+//     ahead,
+//     afoot
+//   }
+}
+
+</script>
+
+
+<style lang = "less" scoped>
+.wid {
+    width: 1200px;
+    margin: 0 auto;
+}
+</style>
