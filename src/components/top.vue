f4cec75e007989a8c4e360df8dc7d951dcdf27d1
shiferqueen <424268118@qq.com>
2017-05-24 10:49:07 +0800

    更改了部分的样式冲突

--- src/components/top.vue
+++ src/components/top.vue
@@ -17,8 +17,7 @@
         <span v-show=false class="fr top-myorder">
           <a href="">我的订单</a>
         </span>
-        <a href="##" class="fr top-shoppingcar">购物车
-          <span class="marg0">{{sum}}</span>件</a>
+        <a href="##" class="fr top-shoppingcar">购物车<span class="marg0">{{sum}}</span>件</a>
       </div>
     </div>
     <!--<p>这是顶部<a href="#/action/register">请注册</a></p>-->
