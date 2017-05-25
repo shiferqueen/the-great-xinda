25a153ca5b61d7120f462fa22f2ac46a22da01a0
OuHuang <365908865@qq.com>
2017-05-25 16:13:07 +0800

    登录注册

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,23 +1,44 @@
 <template>
   <div>
-    <p>这是登录页面</p>
+    手机号<input type="text" v-model="cellphone"><br>
+    密码<input type="text" v-model="password"> <br>
+    图片验证码 <input type="text" v-model="imgcode"> <img @click ='getsrc' :src='imgsrc'><br>
+    <input type="button" value="登录" @click="login">
   </div>
 </template>
 
 <script>
-export default {
-  name: 'login',
-
-}
-
-
-
-
-
-
+    import qs from 'qs'
+    export default {
+        name: 'login',
+        data() {
+            return {
+                imgsrc: "/xinda-api/ajaxAuthcode",
+                cellphone: '',
+                password: '',
+                imgcode: '',
+            }
+        },
+        methods: {
+            getsrc() {
+                this.imgsrc = "/xinda-api/ajaxAuthcode/##";
+            },
+            login() {
+                this.$ajax.post('/xinda-api/sso/login', qs.stringify({
+                    loginId: '' + this.cllphone,
+                    password: '' + this.password,
+                    imgCode: '' + this.imgcode,
+                })).then(function(data) {
+                    console.log(data)
+                })
+            },
+        }
+    }
 </script>
 
 
 <style scoped lang="less">
-
+    input {
+        border: 1px solid #ccc;
+    }
 </style>
