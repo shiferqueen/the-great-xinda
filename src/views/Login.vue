fe20c970cc6703c2a165b8f8dca1bd31b511f770
cigege <cigege>
2017-06-06 11:30:38 +0800

    '超级帅的人修改的超级无敌会员设置'

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -43,8 +43,8 @@
                 testphone:/^1[3|4|5|7|8][0-9]{9}$/,
             }
         },
-        created:{
-            
+        created(){
+
         },
         methods: {
             ...mapActions(["user","refCartNum"]),
